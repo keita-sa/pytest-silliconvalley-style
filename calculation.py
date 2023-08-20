eb850b4cd758d2c7d418b1a0cedb0f09183056c1
Keita Sakurai <58361623+keita-sa@users.noreply.github.com>
2023-08-20 22:22:19 +0900

    Added a file via upload

--- calculation.py
+++ calculation.py
@@ -1,3 +1,4 @@
+import os
 class Cal(object):
     def add_num_and_double(self, x, y):
         if type(x) is not int or type(y) is not int:
@@ -5,3 +6,10 @@ class Cal(object):
         result = x + y
         result *= 2
         return result
+
+    def save(self, dir_path, file_name):
+        if not os.path.exists(dir_path): # True or False
+            os.mkdir(dir_path) # 新しいディレクトリを作成
+        file_path = os.path.join(dir_path, file_name) # dir_path/file_name
+        with open(file_path, 'w') as f:  # ファイルに書き込み
+            f.write('test')
