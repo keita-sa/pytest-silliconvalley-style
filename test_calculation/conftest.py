2b6d63fe936845f8b7307c70890372596fa817fd
Keita Sakurai <keitasakuraims@gmail.com>
2023-08-20 13:22:36 +0900

    Added tmpdir

--- test_calculation/conftest.py
+++ test_calculation/conftest.py
@@ -1,2 +1,8 @@
+import pytest
+
+@pytest.fixture
+def csv_file():
+    return 'csv file!!!'
+
 def pytest_addoption(parser):
     parser.addoption('--os-name', default='linux', help='os name')
--- test_calculation/test_calculation.py
+++ test_calculation/test_calculation.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import calculation
 
@@ -6,12 +7,13 @@ class TestCal(object):
     @classmethod
     def setup_class(cls):
         cls.cal = calculation.Cal()
+        cls.test_file_name = 'test.txt'
 
-    def test_add_num_and_double(self, request):
-        os_name = request.config.getoption('--os-name')
-        print(os_name)
-        if os_name == 'mac':
-            print('ls')
-        elif os_name == 'windows':
-            print('dir')
+    def test_add_num_and_double(self):
         assert self.cal.add_num_and_double(1, 1) == 4
+
+    def test_save(self, tmpdir):
+        self.cal.save(tmpdir, self.test_file_name)
+        test_file_path = os.path.join(
+            tmpdir, self.test_file_name)
+        assert os.path.exists(test_file_path) is True
